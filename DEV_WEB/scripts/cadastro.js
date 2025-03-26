767d7fd775a6b5ac835bca3355970d1f6fde65a3
Gabriel Santos <gabriel.santos@bit7forge.com>
2025-03-26 15:34:18 -0300

    add banco de dados sql

--- DEV_WEB/scripts/cadastro.js
+++ DEV_WEB/scripts/cadastro.js
@@ -1,8 +1,22 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
 // Função para cadastrar um novo usuário
-function cadastrarUsuario(nome, email, senha) {
-  const nome = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("pass").value;
+async function cadastrarUsuario(nome, email, senha) {
+  const db = await open({
+    filename: "./DEV_WEB/banco.db",
+    driver: sqlite3.Database,
+  });
+
+  db.run(
+    `CREATE TABLE IF NOT EXISTS usuarios(id INTEGER PRIMARY KEY, nome TEXT,email TEXT,senha TEXT)`
+  );
+  db.run(
+    `INSERT INTO usuarios (nome, email, senha) VALUES ?,?, `[
+      (nome, email, senha)
+    ]
+  );
+  console.log("Usuário cadastrado com sucesso!");
 }
 
-//Função para verificação de senha
+cadastrarUsuario();
